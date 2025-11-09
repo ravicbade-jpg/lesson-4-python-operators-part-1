0bdee894bd0e5f26e748f4c81c4281b2d49a202b
ravicbade-jpg <ravicbade@gmail.com>
2025-11-09 17:21:16 +0000

    average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+player1 = 23
+player2 = 31
+player3 = 43
+player4 = 45
+player5 = 67
+
+sum = player1+player2+player3+player4+player5
+print("The sum of all the 5 players is: ",sum)
+
+average = sum/5
+print("The average of all the players is: ",average)
